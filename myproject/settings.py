ff9291bad311fee159fe1d17d4c19f061ed43eb6
Jose Aguayo <jose.aguayo@aguayo.co>
2020-11-02 18:34:29 -0500

    Set up django for postgres, created accounts app and routes to register/login/dashboard html

--- myproject/settings.py
+++ myproject/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'pages.apps.PagesConfig',
+    'accounts.apps.AccountsConfig',
 
     'django.contrib.admin',
     'django.contrib.auth',
@@ -77,8 +78,11 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'myprojectdb',
+        'USER': 'postgres',
+        'PASSWORD': 'sunandshield',
+        'HOST': 'localhost'
     }
 }
 
