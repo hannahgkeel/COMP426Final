3195e6d065a820d27ce3923d1e6286c4e51ad55e
Jose Aguayo <jose.aguayo@aguayo.co>
2020-11-02 09:06:51 -0500

    Added topbar and navbar

---
+++ myproject/static/js/main.js
@@ -0,0 +1,21 @@
+
+//This handles the drop down menu on mobile
+$('.navbar-item').each(function(e) {
+    $(this).on('click', function(){
+      if($('#navbar-burger-id').hasClass('is-active')){
+        $('#navbar-burger-id').removeClass('is-active');
+        $('#navbar-menu-id').removeClass('is-active');
+      }
+    });
+  });
+
+  // Open or Close mobile & tablet menu
+  $('#navbar-burger-id').on('click', function () {
+    if($('#navbar-burger-id').hasClass('is-active')){
+      $('#navbar-burger-id').removeClass('is-active');
+      $('#navbar-menu-id').removeClass('is-active');
+    }else {
+      $('#navbar-burger-id').addClass('is-active');
+      $('#navbar-menu-id').addClass('is-active');
+    }
+  });
