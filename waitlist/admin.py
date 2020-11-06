ae6c5b928e5c6b5687a36e657374da2cd84bbc15
Jose Aguayo <jose.aguayo@aguayo.co>
2020-11-06 16:43:15 -0500

    waitlist is accesible from admin site

--- waitlist/admin.py
+++ waitlist/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import WaitlistTicket
 
 # Register your models here.
+class WaitlistAdmin(admin.ModelAdmin):
+    list_display = ('customer', 'party_size', 'checkin_date', 'location')
+    list_filter = ('location',)
+
+
+admin.site.register(WaitlistTicket, WaitlistAdmin)
--- waitlist/models.py
+++ waitlist/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from datetime import datetime
+from django.contrib.auth.models import User
 
 # Create your models here.
+class WaitlistTicket(models.Model):
+    location = models.CharField(max_length=50)
+    checkin_date = models.DateTimeField(default=datetime.now, blank=True)
+    party_size = models.IntegerField()
+    customer = models.CharField(max_length=50)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.customer
