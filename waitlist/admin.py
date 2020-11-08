e74a059bef9e5b0391c98d227393f5de5ab52884
Jose Aguayo <jose.aguayo@aguayo.co>
2020-11-08 10:49:01 -0500

    Hooked up waitlist form to waitlist database

--- waitlist/admin.py
+++ waitlist/admin.py
@@ -2,15 +2,16 @@ from django.contrib import admin
 from .models import WaitlistTicket
 
 # Take care of the next customer:
-def notify_customer(modeladmin, request, queryset):
+def notify_selected_customers(modeladmin, request, queryset):
     queryset.delete()
-    notify_customer.short_description= "Notify customer(s) table is ready"
+    notify_selected_customers.short_description= "Notify selected customers"
 
 # Register your models here.
 class WaitlistAdmin(admin.ModelAdmin):
     list_display = ('customer', 'party_size', 'check_in_time', 'location')
     list_filter = ('location',)
-    actions = [notify_customer]
+    ordering = ('check_in_time',)
+    actions = [notify_selected_customers]
 
 
 admin.site.register(WaitlistTicket, WaitlistAdmin)
